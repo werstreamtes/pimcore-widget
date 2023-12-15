a9670b4789a8ea3ca3100aa122150bf97b2fe127
Andreas Martin <andreas.martin01@funkemedien.de>
2023-12-15 11:15:06 +0100

    update

---
+++ src/Document/Areabrick/Widget.php
@@ -0,0 +1,35 @@
+<?php
+// src/Document/Areabrick/Iframe.php
+
+die("yes");
+
+namespace App\Document\Areabrick;
+
+use Pimcore\Extension\Document\Areabrick\AbstractTemplateAreabrick;
+
+class Widget extends AbstractTemplateAreabrick
+{
+    public function getName()
+    {
+        return 'Widget';
+    }
+
+    public function getDescription()
+    {
+        return 'fooo';
+    }
+
+    public function getTemplateLocation()
+    {
+        return static::TEMPLATE_LOCATION_GLOBAL;
+    }
+
+    public function needsReload(): bool
+    {
+        // optional
+        // here you can decide whether adding this bricks should trigger a reload
+        // in the editing interface, this could be necessary in some cases. default=false
+        return false;
+    }
+
+}
