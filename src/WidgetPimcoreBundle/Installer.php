baf9f3e15dc8548a1a225dfbef54296156640b28
Andreas Martin <andreas.martin01@funkemedien.de>
2023-12-15 11:54:11 +0100

    update

---
+++ src/WidgetPimcoreBundle/Installer.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace WerStreamtEs\WidgetPimcoreBundle;
+
+use Pimcore\Extension\Bundle\Installer\SettingsStoreAwareInstaller;
+
+class Installer extends SettingsStoreAwareInstaller
+{
+}
