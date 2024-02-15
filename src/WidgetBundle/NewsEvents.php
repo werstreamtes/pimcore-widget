83d0847dd3891b7c8ac2575a691823c2b4fc274b
Andreas Martin <andreas.martin01@funkemedien.de>
2024-02-15 14:44:02 +0100

    update

--- src/WidgetBundle/NewsEvents.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace WidgetBundle;
-
-class NewsEvents
-{
-    public const NEWS_BRICK_QUERY_BUILD = 'news.brick.query_build';
-    public const NEWS_EDITABLE_DIALOG = 'news.editable.dialog';
-}
