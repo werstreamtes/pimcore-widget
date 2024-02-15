bb3d9a4becf914aa6401465396194a91f8195b33
Andreas Martin <andreas.martin01@funkemedien.de>
2024-02-15 13:49:01 +0100

    update

--- src/DependencyInjection/WidgetPimcoreBundleExtension.php
+++ src/DependencyInjection/WidgetPimcoreBundleExtension.php
@@ -1,39 +1,29 @@
 <?php
-declare(strict_types=1);
 
-/**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
- */
+namespace WseWidget\DependencyInjection;
 
-namespace WerStreamtEs\WidgetPimcoreBundle\DependencyInjection;
-
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use NewsBundle\Configuration\Configuration as BundleConfiguration;
 
-final class WidgetPimcoreBundleExtension extends ConfigurableExtension
+class WidgetPimcoreBundleExtension extends Extension
 {
-    public function loadInternal(array $config, ContainerBuilder $container): void
+    public function load(array $configs, ContainerBuilder $container): void
     {
-        // on container build the shutdown handler shouldn't be called
-        // for details please see https://github.com/pimcore/pimcore/issues/4709
-        \Pimcore::disableShutdown();
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__ . '/../../config')
-        );
+        $loader = new YamlFileLoader($container, new FileLocator([__DIR__ . '/../Resources/config']));
+        $loader->load('services.yml');
 
-        $loader->load('services.yaml');
+        foreach ($config['relations'] as $confName => $confValue) {
+            $definition = $container->findDefinition($confName);
+            $definition->setClass($confValue);
+        }
+
+        $configManagerDefinition = $container->getDefinition(BundleConfiguration::class);
+        $configManagerDefinition->addMethodCall('setConfig', [$config]);
     }
 }
