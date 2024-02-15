e3a356cacb4aaf3da42e69df608455f8b075feb5
Andreas Martin <andreas.martin01@funkemedien.de>
2024-02-15 14:08:32 +0100

    update

---
+++ src/WSEWidget/Configuration/Configuration.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace NewsBundle\Configuration;
+
+class Configuration
+{
+    public const SYSTEM_CONFIG_DIR_PATH = PIMCORE_PRIVATE_VAR . '/bundles/WSEWidget';
+    public const SYSTEM_CONFIG_FILE_PATH = PIMCORE_PRIVATE_VAR . '/bundles/WSEWidget/config.yml';
+
+    protected array $config;
+
+    public function setConfig(array $config = []): void
+    {
+        $this->config = $config;
+    }
+
+    public function getConfigNode(): array
+    {
+        return $this->config;
+    }
+
+    public function getConfigArray(): array
+    {
+        return $this->config;
+    }
+
+    public function getConfig(string $slot): mixed
+    {
+        return $this->config[$slot];
+    }
+}
