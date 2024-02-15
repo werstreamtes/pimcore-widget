dae3ab9a9d4eadc79d416b16f19a33e260b5c98d
Andreas Martin <andreas.martin01@funkemedien.de>
2024-02-15 14:50:19 +0100

    update

--- src/DependencyInjection /WerStreamtEsWidgetBundleExtension.php
+++ src/DependencyInjection /WerStreamtEsWidgetBundleExtension.php
@@ -22,7 +22,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class PimcoreBundleGeneratorExtension extends Extension
+class WerStreamtEsWidgetBundleExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
